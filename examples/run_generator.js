76aa036a4dda874a025ad4a7ae86d5c4804e6bdb
Ian Copp <copp.ian@gmail.com>
2016-03-09 14:32:13 -0800

    Add another example file

--- examples/run_generator.js
+++ examples/run_generator.js
@@ -1,9 +1,16 @@
+#!/usr/bin/env node
+
 var Tablesmeerp = require('../tablesmeerp');
 var fs = require('fs');
 
+var args = process.argv.slice(2);
+if (!args[0]) {
+  return;
+}
+
 var yamltext = '';
 try {
-  yamltext = fs.readFileSync(__dirname + '/world_design.yaml', 'utf8');
+  yamltext = fs.readFileSync(args[0], 'utf8');
 } catch (e) {
   console.log('Failed to load YAML file.');
 }
