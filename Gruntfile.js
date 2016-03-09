d29c38aae5a5b604c992dfab2441c40b85582495
Ian Copp <copp.ian@gmail.com>
2016-03-09 14:55:25 -0800

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    bump: {},
+    jshint: {
+      files: ['Gruntfile.js', 'index.js', 'bin/**/*.js']
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.registerTask('default', ['jshint']);
+};
